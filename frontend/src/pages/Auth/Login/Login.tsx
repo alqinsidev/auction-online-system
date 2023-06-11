99b806a7acf2e7f6b07da7d0732e4f1f57e129db
alqinsidev <padlanalqinsi@live.com>
2023-06-11 09:40:10 +0700

    add(Login): Remember me feature

--- frontend/src/pages/Auth/Login/Login.tsx
+++ frontend/src/pages/Auth/Login/Login.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Alert, Button, Col, Row, Space, Typography } from 'antd'
+import { useState, useEffect } from 'react'
+import { Alert, Button, Checkbox, Col, Row, Space, Typography } from 'antd'
 import { TextInput } from '../../../components'
 import { useFormik } from 'formik'
 import AuthServices from '../../../services/auth.service'
@@ -8,18 +8,34 @@ import HandleError from '../../../utils/errorHandler.utils'
 import loginSchema from '../../../common/validation/auth.validation'
 import { useNavigate } from 'react-router-dom'
 import { ErrorData } from '../../../common/interface/error.interface'
+import { CheckboxChangeEvent } from 'antd/es/checkbox'
 const { Title } = Typography
 
 const Login = () => {
   const navigate = useNavigate()
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [hasError, setHasError] = useState<ErrorData | null>(null)
+  const [rememberMe, setRememberMe] = useState<boolean>(false)
+
+  const storedEmail = localStorage.getItem('email') || ''
+
+  useEffect(() => {
+    const storedRememberMe = localStorage.getItem('rememberMe') || 'false'
+    setRememberMe(storedRememberMe === 'true')
+  }, [])
 
   const handleSubmit = async (values: AuthPayload) => {
     setIsLoading(true)
     try {
       await AuthServices.signIn(values)
       navigate('/home')
+      if (rememberMe) {
+        localStorage.setItem('email', values.email)
+        localStorage.setItem('rememberMe', 'true')
+      } else {
+        localStorage.removeItem('email')
+        localStorage.removeItem('rememberMe')
+      }
     } catch (error) {
       setHasError(HandleError(error))
     } finally {
@@ -29,13 +45,17 @@ const Login = () => {
 
   const formik = useFormik({
     initialValues: {
-      email: '',
+      email: storedEmail && storedEmail !== '' ? storedEmail : '', 
       password: '',
     },
     validationSchema: loginSchema,
     validateOnChange: false,
     onSubmit: handleSubmit
   })
+
+  const handleRememberMeChange = (e: CheckboxChangeEvent) => {
+    setRememberMe(e.target.checked)
+  }
 
   return (
     <Row justify={'center'} align={'middle'} style={{ minHeight: '100vh' }}>
@@ -49,6 +69,7 @@ const Login = () => {
             <Space direction='vertical' size={'middle'} style={{ width: '100%' }}>
               <TextInput label='Email' value={formik.values.email} onChange={formik.handleChange('email')} errorMessage={formik.errors.email} />
               <TextInput secure label='Password' value={formik.values.password} onChange={formik.handleChange('password')} errorMessage={formik.errors.password} />
+              <Checkbox checked={rememberMe} onChange={handleRememberMeChange}>Remember Me</Checkbox>
               <Button loading={isLoading} htmlType='submit' type='primary' block>Login</Button>
             </Space>
           </form>
